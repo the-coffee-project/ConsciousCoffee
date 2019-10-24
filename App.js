0eac1b380ff11b0bdfe0f6a3db84e6c80ca51bf6
Luke Mujica <lukemujica@berkeley.edu>
2019-10-24 16:21:05 -0700

    initial commit

--- App.js
+++ App.js
@@ -4,43 +4,20 @@
  *
  * @format
  * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 import CafeListScreen from 'screens/CafeListScreen';
 import CafeDetailsScreen from 'screens/CafeDetailsScreen';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 import {createStackNavigator, createAppContainer} from "react-navigation";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-/*
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-*/
 
 const AppNavigator = createStackNavigator({
     Home: {
-      screen: CafeListScreen
+      screen: HelloWorldScreen
     },
     Details: {
-      screen: CafeDetailsScreen
+      screen: HelloWorldScreen
     },
   },
   {
@@ -51,35 +28,16 @@ const AppNavigator = createStackNavigator({
       },
       headerTintColor: '#000',
       headerTitleStyle: {
-        
+
       },
     },
   }
 );
 
-const AppContainer = createAppContainer(AppNavigator);
+const App: () => React$Node = () => {
+  return (
+    <AppContainer />
+  );
+};
 
-export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
